39366be02d8b232ec7f6cb94f52186bf59e3bdfa
Rmparker94 <Rmparker94@gmail.com>
2015-05-05 22:43:10 -0400

    Create Commons.java

---
+++ Commons.java
@@ -0,0 +1,15 @@
+package main;
+/*
+ * Original Game developed by Atari Inc.
+ * 	Re-designed by Jan Bodnar http://zetcode.com/tutorials/javagamestutorial/breakout/
+ * 		Further modified and edited by Reid Parker
+ * 			Version 1.0 - May 5, 2015 -BugBuster-
+ */
+
+public interface Commons {
+    public static final int WIDTH = 700;
+    public static final int HEIGHT = 800;
+    public static final int BOTTOM = 780;
+    public static final int PADDLE_RIGHT = 590;
+    public static final int BALL_RIGHT = 680;
+}
